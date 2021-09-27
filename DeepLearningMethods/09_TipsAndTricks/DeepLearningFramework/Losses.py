012051f5b14629774f5b4823011a9db4ff6f0878
Or Yair <oryair@fixelalgorithms.com>
2021-09-27 23:29:42 +0300

    Edit TipsAndTircks

---
+++ DeepLearningMethods/09_TipsAndTricks/DeepLearningFramework/Losses.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+#-- https://github.com/pytorch/pytorch/issues/7455
+class LabelSmoothingLoss(nn.Module):
+    def __init__(self, nClass, ε=0.01):
+        super(LabelSmoothingLoss, self).__init__()
+        self.nClass = nClass
+        self.ε      = ε
+
+    def forward(self, mScore, vY):
+        ε      = self.ε
+        nClass = self.nClass
+        mScore = mScore.log_softmax(dim=-1)
+
+        with torch.no_grad():
+            mSmoothY = torch.empty_like(mScore).fill_(ε / (nClass - 1))
+            mSmoothY.scatter_(1, vY.unsqueeze(1), 1-ε)
+
+        return torch.mean( torch.sum(-mSmoothY * mScore, dim=-1) )
---
+++ DeepLearningMethods/09_TipsAndTricks/DeepLearningFramework/Training.py
@@ -0,0 +1,153 @@
+import numpy as np
+import time
+import torch
+
+from DeepLearningFramework.Metric import Accuracy, R2Score
+
+#--------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------#
+def Epoch(oModel, oDataDL, Loss, Metric, oOptim=None, oScheduler=None, bTrain=True):
+
+    epochLoss   = 0
+    epochMetric = 0
+    count       = 0
+    nIter       = len(oDataDL)
+    vLR         = np.full(nIter, np.nan)
+    DEVICE      = next(oModel.parameters()).device #-- CPU\GPU
+
+
+    oModel.train(bTrain) #-- train or test
+
+    #-- Iterate over the mini-batches:
+    for ii, (mX, vY) in enumerate(oDataDL):
+        #-- Move to device (CPU\GPU):
+        mX = mX.to(DEVICE)
+        vY = vY.to(DEVICE)
+
+        #-- Forward:
+        if bTrain == True:
+            #-- Store computational graph:
+            mZ   = oModel(mX)
+            loss = Loss(mZ, vY)
+        else:
+            with torch.no_grad():
+                #-- Do not store computational graph:
+                mZ   = oModel(mX)
+                loss = Loss(mZ, vY)
+
+        #-- Backward:
+        if bTrain == True:
+            oOptim.zero_grad() #-- set gradients to zeros
+            loss.backward()    #-- backward
+            oOptim.step()      #-- update parameters
+            if oScheduler is not None:
+                vLR[ii] = oScheduler.get_last_lr()[0]
+                oScheduler.step() #-- update learning rate
+
+        Nb           = vY.shape[0]
+        count       += Nb
+        epochLoss   += Nb * loss.item()
+        epochMetric += Nb * Metric(mZ, vY)
+        print(f'\r{"Train" if bTrain else "Val"} - Iteration: {ii:3d} ({nIter}): loss = {loss:2.6f}', end='')
+
+    print('', end='\r')
+    epochLoss   /= count
+    epochMetric /= count
+
+    return epochLoss, epochMetric, vLR
+
+#--------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------#
+def TrainClassficationModel(oModel, oTrainData, oValData, Loss, nEpochs, oOptim, oScheduler=None):
+
+    vTrainLoss = np.full(nEpochs, np.nan)
+    vTrainAcc  = np.full(nEpochs, np.nan)
+    vValLoss   = np.full(nEpochs, np.nan)
+    vValAcc    = np.full(nEpochs, np.nan)
+    vLR        = np.full(0,       np.nan)
+    bestAcc    = 0
+
+    for epoch in range(nEpochs):
+        startTime                 = time.time()
+        trainLoss, trainAcc, vLRi = Epoch(oModel, oTrainData, Loss, Accuracy, oOptim, oScheduler, bTrain=True ) #-- train
+        valLoss,   valAcc,   _    = Epoch(oModel, oValData,   Loss, Accuracy,                     bTrain=False) #-- validate
+        epochTime                 = time.time() - startTime
+
+        #-- Display:
+        print('Epoch '              f'{epoch    :03d}:',   end='')
+        print(' | Train loss: '     f'{trainLoss:.5f}' ,   end='')
+        print(' | Val loss: '       f'{valLoss  :.5f}' ,   end='')
+        print(' | Train Accuracy: ' f'{trainAcc :2.4f}',   end='')
+        print(' | Val Accuracy: '   f'{valAcc   :2.4f}',   end='')
+        print(' | epoch time: '     f'{epochTime:6.3f} |', end='')
+
+        vTrainLoss[epoch] = trainLoss
+        vTrainAcc [epoch] = trainAcc
+        vValLoss  [epoch] = valLoss
+        vValAcc   [epoch] = valAcc
+        vLR               = np.concatenate([vLR, vLRi])
+
+        #-- Save best model (early stopping):
+        if bestAcc < valAcc:
+            bestAcc = valAcc
+            try:
+                torch.save(oModel.state_dict(), 'BestModel.pt')
+            except:
+                pass
+            print(' <-- Checkpoint!')
+        else:
+            print('')
+
+    #-- Load best model (early stopping):
+    oModel.load_state_dict(torch.load('BestModel.pt'))
+
+    return vTrainLoss, vTrainAcc, vValLoss, vValAcc, vLR
+
+#--------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------#
+def TrainRegressionModel(oModel, oTrainData, oValData, Loss, nEpochs, oOptim, oScheduler=None):
+
+    vTrainLoss = np.full(nEpochs, np.nan)
+    vTrainR2   = np.full(nEpochs, np.nan)
+    vValLoss   = np.full(nEpochs, np.nan)
+    vValR2     = np.full(nEpochs, np.nan)
+    vLR        = np.full(0,       np.nan)
+    bestR2     = -np.inf
+
+    for epoch in range(nEpochs):
+        startTime                = time.time()
+        trainLoss, trainR2, vLRi = Epoch(oModel, oTrainData, Loss, R2Score, oOptim, oScheduler, bTrain=True ) #-- train
+        valLoss,   valR2,   _    = Epoch(oModel, oValData,   Loss, R2Score,                     bTrain=False) #-- validate
+        epochTime                = time.time() - startTime
+
+        #-- Display:
+        print('Epoch '          f'{epoch    :03d}:',   end='')
+        print(' | Train loss: ' f'{trainLoss:.5f}' ,   end='')
+        print(' | Val loss: '   f'{valLoss  :.5f}' ,   end='')
+        print(' | Train R2: '   f'{trainR2 :+2.4f}',   end='')
+        print(' | Val R2: '     f'{valR2   :+2.4f}',   end='')
+        print(' | epoch time: ' f'{epochTime:6.3f} |', end='')
+
+        vTrainLoss[epoch] = trainLoss
+        vTrainR2  [epoch] = trainR2
+        vValLoss  [epoch] = valLoss
+        vValR2    [epoch] = valR2
+        vLR               = np.concatenate([vLR, vLRi])
+
+        #-- Save best model (early stopping):
+        if bestR2 < valR2:
+            bestR2 = valR2
+            try:
+                torch.save(oModel.state_dict(), 'BestModel.pt')
+            except:
+                pass
+            print(' <-- Checkpoint!')
+        else:
+            print('')
+
+    #-- Load best model (early stopping):
+    oModel.load_state_dict(torch.load('BestModel.pt'))
+
+    return vTrainLoss, vTrainR2, vValLoss, vValR2, vLR
+#--------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------#
