2a764978a94ea16f6148c647762a5c6429df3a17
Or Yair <oryair@fixelalgorithms.com>
2021-09-28 21:13:04 +0300

    Edit label smoothing

--- DeepLearningMethods/09_TipsAndTricks/DeepLearningFramework/Training.py
+++ DeepLearningMethods/09_TipsAndTricks/DeepLearningFramework/Training.py
@@ -2,7 +2,7 @@ import numpy as np
 import time
 import torch
 
-from DeepLearningFramework.Metric import Accuracy, R2Score
+from .Metric import Accuracy, R2Score
 
 #--------------------------------------------------------------------------------#
 #--------------------------------------------------------------------------------#
