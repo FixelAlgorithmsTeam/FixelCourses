00f2f2ad502c733518d816eae9ff1b08172791bf
Royi <RoyiAvital@fixelalgorithms.com>
2024-07-16 16:48:30 +0300

    AI Program
     - Updated: Workshop 003.

---
+++ AIProgram/2024_02/WorkShop003/DataLoader.py
@@ -0,0 +1,112 @@
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torchvision.datasets.vision import VisionDataset  
+from torchvision.transforms.functional import pil_to_tensor, to_tensor
+
+import skimage as ski
+from PIL import Image
+
+# Miscellaneous
+import os
+import pathlib
+
+from typing import Any, Callable, List, Optional, Union, Tuple
+
+def ExtFileName( fullFileName: str ) -> str:
+
+    fileName, fileExt = os.path.splitext(fullFileName)
+
+    return fileName
+
+class ImageSegmentationDataset(VisionDataset):
+    def __init__(self, datasetPath: str, imgTransform: Callable = None, maskTransform: Callable = None, maskDType = np.int64, lImgFormats: List = ['jpg', 'jpeg', 'png']):
+        super().__init__(datasetPath, transforms = None, transform = imgTransform, target_transform = maskTransform)
+        
+        self.maskDType = maskDType
+        self.imgFolderPath = os.path.join(self.root, 'Images')
+        self.annFolderPath  = os.path.join(self.root, 'Annotations')
+
+        lAnnFileName = [ExtFileName(itmName) for itmName in os.listdir(self.annFolderPath)]
+        lImgFileName = [ExtFileName(itmName) for itmName in os.listdir(self.imgFolderPath)]
+
+        sMatchFileName =  set(lAnnFileName) & set(lImgFileName)
+
+        _lAnn = [] #<! Annotations
+        _lImg = [] #<! Images
+
+        for fileItm in os.listdir(self.imgFolderPath):
+            fileName, fileExt = os.path.splitext(fileItm)
+            if ((fileExt[1:] in lImgFormats) and (fileName in sMatchFileName)):
+                _lImg.append(os.path.join(self.imgFolderPath, fileItm))
+                _lAnn.append(os.path.join(self.annFolderPath, fileName + '.png'))
+
+        self._lAnn = _lAnn
+        self._lImg = _lImg
+
+    def __len__(self) -> int:
+        return len(self._lImg)
+    
+    def __getitem__(self, idx: int) -> Tuple[Any, Any]:
+
+        # On PyTorch 2.3.1 (Checked only on Windows) `imread()` caused issues
+        # mI = ski.io.imread(self._lImg[idx])
+        # mM = ski.io.imread(self._lAnn[idx])
+
+        mI = np.array(Image.open(self._lImg[idx]).convert("RGB")) #<! Guarantees 3 channels
+        mM = np.array(Image.open(self._lAnn[idx]).convert("L")) #<! Guarantees 1 channels
+        mM = mM - 1
+
+        # if (np.ndim(mI) < 3):
+        #     # Grayscale Image
+        #     print('2D')
+        #     mI = mI[:, :, None]
+
+        # if (np.size(mI, 2) == 1):
+        #     # Grayscale Image
+        #     print('Gray')
+        #     mI = np.tile(mI, (1, 1, 3))
+        
+        # if (np.size(mI, 2) == 4):
+        #     # RGBA Image
+        #     print('!!!!!RGBA!!!!!')
+        #     mI = mI[:, :, :3]
+        
+        if self.transform is not None:
+            mI = self.transform(mI)
+        
+        if self.target_transform is not None:
+            mM = self.target_transform(mM)
+        
+        # print(f'Image Index: {idx}')
+        # print(f'Image shape: {mI.shape}')
+        # print(f'Target shape: {mM.shape}')
+
+        return mI, mM
+
+
+def GenTrainTesIdx(numSamples: int, trainSize: float = 0.8, seedNum: int = 123):
+    
+    vAllIdx         = np.arange(numSamples)
+    numTainsSamples = int(trainSize * numSamples)
+    
+    rng = np.random.default_rng(seedNum) #<! Stable Random Number Generator
+    
+    vTrainIdx   = rng.choice(numSamples, numTainsSamples, replace = False) 
+    vTestIdx    = np.setdiff1d(vAllIdx, vTrainIdx)
+
+    vTrainIdx   = np.sort(vTrainIdx)
+    vTestIdx    = np.sort(vTestIdx)
+
+    return vTrainIdx, vTestIdx
+
+class AdjustMask(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, x):
+        
+        return torch.squeeze(x) - 1
+
---
+++ AIProgram/2024_02/WorkShop003/UNetModule.py
@@ -0,0 +1,125 @@
+
+
+import torch
+import torch.nn as nn
+
+from enum import Enum, auto, unique
+
+from typing import Any, Callable, Dict, Generator, List, Optional, Self, Set, Tuple, Union
+
+
+class DoubleConv(nn.Module):
+    """(Convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels = None, kernel_size = 3):
+        super().__init__()
+        if mid_channels is None:
+            mid_channels = out_channels
+            
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size = kernel_size, padding = 'same', bias = False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size = kernel_size, padding = 'same', bias = False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace = True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with MaxPool then double Conv"""
+
+    def __init__(self, in_channels, out_channels, kernel_size = 3):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels, kernel_size = kernel_size)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double Conv"""
+
+    def __init__(self, in_channels, out_channels, kernel_size = 3):
+        super().__init__()
+
+        self.up     = nn.ConvTranspose2d(in_channels, out_channels, kernel_size = 2, stride = 2, padding = 0)
+        self.conv   = DoubleConv(2 * out_channels, out_channels, kernel_size) #<! Royi: Assumption, the number of channels in filters are mult by 2
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x = torch.cat([x2, x1], dim = 1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size = 1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class BuildUNet(nn.Module):
+    def __init__(self, n_channels, n_classes, filter_size, *, kernel_size = 3, top_layer = None):
+        super(BuildUNet, self).__init__()
+        # Assumption: filter_size[ii + 1] == 2 * filter_size[ii]
+        # TODO: fix the above assumption by sending the actual size to the Up layer.
+        # TODO: Check the code for small number of filters (1).
+        # TODO: Works for input with even dimensions only
+        
+        
+        self.n_channels     = n_channels
+        self.n_classes      = n_classes
+        self.num_filters    = len(filter_size)
+        self.filter_size    = filter_size
+
+        self.inc = DoubleConv(n_channels, filter_size[0], kernel_size = kernel_size)
+        print(f'In Layer: {filter_size[0]}')
+        down_layer = []
+        for ii in range(1, self.num_filters):
+            down_layer.append(Down(filter_size[ii - 1], filter_size[ii], kernel_size = kernel_size))
+            print(f'Down Layer: {filter_size[ii]}')
+        
+        up_layer = []
+        for ii in range(1, self.num_filters):
+            up_layer.append(Up(filter_size[-ii], filter_size[-(ii + 1)], kernel_size = kernel_size))
+            print(f'Up Layer: {filter_size[-ii]}')
+
+        # `nn.ModuleList` vs. `nn.Sequential` : https://stackoverflow.com/questions/47544051
+        self.down_layer = nn.ModuleList(down_layer)
+        self.up_layer   = nn.ModuleList(up_layer)
+
+        self.outc = OutConv(filter_size[0], n_classes)
+        print(f'Out Layer: {filter_size[0]}')
+
+        self.buff = [None] * self.num_filters
+
+        if top_layer is None:
+            self.top_layer = nn.Identity()
+        else:
+            self.top_layer = top_layer
+
+    def forward(self, x) -> torch.Tensor:
+        
+        self.buff[0] = self.inc(x)
+        for ii in range(1, self.num_filters):
+            self.buff[ii] = self.down_layer[ii - 1](self.buff[ii - 1])
+        
+        x = self.up_layer[0](self.buff[-1], self.buff[-2])
+        for ii in range(1, self.num_filters - 1):
+            x = self.up_layer[ii](x, self.buff[-(ii + 2)])
+
+        out_layer = self.outc(x)
+        out_layer = self.top_layer(out_layer)
+        
+        return out_layer
+
+
