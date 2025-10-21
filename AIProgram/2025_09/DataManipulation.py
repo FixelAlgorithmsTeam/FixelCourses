82437b8f771e402e6a6d8cfe3bab4598152735d9
Royi <RoyiAvital@fixelalgorithms.com>
2025-10-21 19:00:34 +0300

    AI Program
     - Updated: Added notebooks for 2025_09.

---
+++ AIProgram/2025_09/DataManipulation.py
@@ -0,0 +1,199 @@
+
+# Python STD
+from enum import auto, Enum, unique
+# import math
+import shutil
+
+# Data
+import numpy as np
+# import pandas as pd
+# import scipy as sp
+
+# Machine Learning
+
+# Image Processing / Computer Vision
+import skimage as ski
+
+# Optimization
+
+# Auxiliary
+
+# Visualization
+
+# Miscellaneous
+import gdown
+import gzip
+import os
+import urllib.request
+
+# Typing
+from typing import Callable, Dict, List, Optional, Set, Tuple, Union
+
+# See https://docs.python.org/3/library/enum.html
+@unique
+class DiffMode(Enum):
+    # Type of data in the CSV
+    BACKWARD    = auto()
+    CENTRAL     = auto()
+    FORWARD     = auto()
+    COMPLEX     = auto()
+
+@unique
+class BBoxFormat(Enum):
+    # Bounding Box Format, See https://albumentations.ai/docs/getting_started/bounding_boxes_augmentation
+    COCO        = auto()
+    PASCAL_VOC  = auto()
+    YOLO        = auto()
+
+# Constants
+L_ARCHIVE_EXT = ['.zip', '.tar.bz2', '.bz2', '.tbz2', '.tar.gz', '.gz', '.tgz', '.tar', '.tar.xz', '.xz', '.txz']
+
+def DownloadGDriveZip( fileId: str, lFileCont: List[str] ) -> None:
+
+    for fileName in lFileCont:
+        if os.path.isfile(fileName):
+            os.remove(fileName)
+    
+    fileNameExt = gdown.download(id = fileId)
+    fileName, fileExt = os.path.splitext(fileNameExt)
+    if fileExt in L_ARCHIVE_EXT:
+        # Might not work with `tar` files (Might require unpacking twice)
+        shutil.unpack_archive(fileNameExt)
+        os.remove(fileNameExt)
+
+def DownloadDecompressGzip( fileUrl: str, fileName: str ) -> None:
+    # Based on https://stackoverflow.com/a/61195974
+
+    # Read the file inside the .gz archive located at url
+    with urllib.request.urlopen(fileUrl) as response:
+        with gzip.GzipFile(fileobj = response) as uncompressed:
+            file_content = uncompressed.read()
+        # write to file in binary mode 'wb'
+        with open(fileName, 'wb') as f:
+            f.write(file_content)
+            f.close()
+        return
+
+def ParseEnvFile( fileName: str = '.env', *, filePath: str = '.', keyValSep: str = '=' ) -> Dict[str, str]:
+    # Read the file and parse it into a dictionary
+    dEnv = {}
+    with open(os.path.join(filePath, fileName), 'r') as hF:
+        for line in hF:
+            if line.startswith('#') or not line.strip():
+                continue
+            key, value = line.strip().split(keyValSep, 1)
+            dEnv[key]  = value.strip()
+    return dEnv
+
+def DownloadUrl( fileUrl: str, fileName: str ) -> str:
+    
+    if not os.path.exists(fileName):
+        urllib.request.urlretrieve(fileUrl, fileName)
+
+    return fileName
+
+def ConvertMnistDataDf( imgFilePath: str, labelFilePath: str ) -> Tuple[np.ndarray, np.ndarray]:
+    numPx = 28 * 28
+    # Merge of https://pjreddie.com/projects/mnist-in-csv/ and https://github.com/keras-team/keras/blob/master/keras/datasets/fashion_mnist.py
+    f = open(imgFilePath, "rb")
+    l = open(labelFilePath, "rb")
+
+    lCol = [f'Px {ii:04}' for ii in range (numPx)]
+    lCol.append('Label')
+
+    vY = np.frombuffer(l.read(), np.uint8, offset = 8)
+    mX = np.frombuffer(f.read(), np.uint8, offset = 16)
+    # mX = np.reshape(mX, (numPx, len(vY))).T
+    mX = np.reshape(mX, (len(vY), numPx))
+
+    f.close()
+    l.close()
+
+    return mX, vY
+
+def ConvertBBoxFormat( vBox: np.ndarray, tuImgSize: Tuple[int, int], boxFormatIn: BBoxFormat, boxFormatOut: BBoxFormat ) -> np.ndarray:
+    # tuImgSize = (numRows, numCols) <=> (imgHeight, imgWidth)
+
+    vB = vBox.copy()
+    
+    # COCO = [xMin, yMin, boxWidth, boxHeight]
+    if ((boxFormatIn == BBoxFormat.COCO) and (boxFormatOut == BBoxFormat.PASCAL_VOC)):
+        vB[2] += vB[0] #<! xMax = Width + xMin
+        vB[3] += vB[1] #<! yMax = Height + yMin
+    elif ((boxFormatIn == BBoxFormat.COCO) and (boxFormatOut == BBoxFormat.YOLO)):
+        vB[0] += (vB[2] / 2)  #<! xCenter = xMin + (boxWidth / 2)
+        vB[1] += (vB[3] / 2)  #<! yCenter = yMin + (boxHeight / 2)
+        vB[0] /= tuImgSize[1] #<! xCenter / imgWidth
+        vB[1] /= tuImgSize[0] #<! yCenter / imgHeight
+        vB[2] /= tuImgSize[1] #<! boxWidth / imgWidth
+        vB[3] /= tuImgSize[0] #<! boxHeight / imgHeight
+    
+    # PASCAL_VOC = [xMin, yMin, xMax, yMax]
+    elif ((boxFormatIn == BBoxFormat.PASCAL_VOC) and (boxFormatOut == BBoxFormat.COCO)):
+        vB[2] -= vB[0] #<! boxWidth  = xMax - xMin
+        vB[3] -= vB[1] #<! boxHeight = yMax - yMin
+    elif ((boxFormatIn == BBoxFormat.PASCAL_VOC) and (boxFormatOut == BBoxFormat.YOLO)):
+        vB[0] = (vB[0] + vB[2]) / 2                 #<! xCenter = (xMin + xMax) / 2
+        vB[1] = (vB[1] + vB[3]) / 2                 #<! yCenter = (yMin + yMax) / 2
+        vB[0] /= tuImgSize[1]                       #<! xCenter / imgWidth
+        vB[1] /= tuImgSize[0]                       #<! yCenter / imgHeight
+        vB[2] = (vBox[2] - vBox[0]) / tuImgSize[1]  #<! boxWidth = (xMax - xMin) / imgWidth
+        vB[3] = (vBox[3] - vBox[1]) / tuImgSize[0]  #<! boxHeight = (YMax - yMin) / imgHeight
+    
+    # YOLO = [xCenter, yCenter, boxWidth, boxHeight] (Normalized)
+    elif ((boxFormatIn == BBoxFormat.YOLO) and (boxFormatOut == BBoxFormat.COCO)):
+        vB[0] -= (vB[2] / 2.0) #!< xMin = xCenter - (boxWidth / 2)
+        vB[1] -= (vB[3] / 2.0) #!< yMin = yCenter - (boxHeight / 2)
+        vB[0] *= tuImgSize[1]  #<! xMin * imgWidth
+        vB[1] *= tuImgSize[0]  #<! yMin * imgHeight
+        vB[2] *= tuImgSize[1]  #<! boxWidth * imgWidth
+        vB[3] *= tuImgSize[0]  #<! boxHeight * imgHeight
+    elif ((boxFormatIn == BBoxFormat.YOLO) and (boxFormatOut == BBoxFormat.PASCAL_VOC)):
+        vB[0] -= (vB[2] / 2.0) #!< xMin = xCenter - (boxWidth / 2)
+        vB[1] -= (vB[3] / 2.0) #!< yMin = yCenter - (boxHeight / 2)
+        vB[2] += vB[0]         #<! xMax = boxWidth + xMin
+        vB[3] += vB[1]         #<! yMax = boxHeight + yMin
+        vB[0] *= tuImgSize[1]  #<! xMin * imgWidth
+        vB[1] *= tuImgSize[0]  #<! yMin * imgHeight
+        vB[2] *= tuImgSize[1]  #<! xMax * imgWidth
+        vB[3] *= tuImgSize[0]  #<! yMax * imgHeight
+    
+    return vB
+
+def GenLabeldEllipseImg( tuImgSize: Tuple[int, int], numObj: int, *, boxFormat: BBoxFormat = BBoxFormat.YOLO ) -> Tuple[np.ndarray, np.ndarray]:
+    # Image Size in Rows x Cols
+
+    mI  = np.zeros(shape = (*tuImgSize, 3)) #<! RGB Image
+    vY  = np.zeros(shape = numObj, dtype = np.int_)
+    mBB = np.zeros(shape = (numObj, 4)) #<! [x1, y1, x2, y2]
+
+    for ii in range(numObj):
+        cIdx    = np.random.randint(3)     #<! R, G, B -> [0, 1, 2]
+        rotDeg  = np.pi * np.random.rand() #<! [0, π]
+        centRow = np.random.randint(low = int(np.ceil(0.1 * tuImgSize[0])), high = int(np.ceil(0.9 * tuImgSize[0])))
+        centCol = np.random.randint(low = int(np.ceil(0.1 * tuImgSize[1])), high = int(np.ceil(0.9 * tuImgSize[1])))
+        majAxis = (tuImgSize[0] / 16) + ((tuImgSize[0] / 4) * np.random.rand()) #<! Major Axis
+        minAxis = (tuImgSize[1] / 16) + ((tuImgSize[1] / 4) * np.random.rand()) #<! Minor Axis
+
+        # Generate the Ellipse
+        vR, vC = ski.draw.ellipse(centRow, centCol, majAxis, minAxis, shape = tuImgSize, rotation = rotDeg)
+
+        mI[vR, vC, cIdx] = 1.0 #<! Class of the ellipse
+
+        # Bounding Box
+        xLeft   = np.min(vC)
+        xRight  = np.max(vC)
+        yTop    = np.min(vR)
+        yBottom = np.max(vR)
+
+        # PASCAL VOC format
+        vY[ii]     = cIdx    #<! Label
+        mBB[ii, 0] = xLeft   #<! x Min
+        mBB[ii, 1] = yTop    #<! y Min
+        mBB[ii, 2] = xRight  #<! x Max
+        mBB[ii, 3] = yBottom #<! y Max
+
+        if (boxFormat != BBoxFormat.PASCAL_VOC):
+            mBB[ii] = ConvertBBoxFormat(mBB[ii], tuImgSize, BBoxFormat.PASCAL_VOC, boxFormat)
+
+    return mI, vY, mBB
---
+++ AIProgram/2025_09/DataVisualization.py
@@ -0,0 +1,397 @@
+
+# Python STD
+# import enum
+import math
+
+# Data
+import numpy as np
+import pandas as pd
+import scipy as sp
+
+# Machine Learning
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+# Image Processing / Computer Vision
+
+# Optimization
+
+# Auxiliary
+from numba import jit, njit
+
+# Visualization
+import distinctipy
+import matplotlib.colors
+from matplotlib.patches import Rectangle, Circle
+import matplotlib.pyplot as plt
+
+# Miscellaneous
+from enum import auto, Enum, unique
+
+# Typing
+from typing import Callable, Dict, List, Optional, Set, Tuple, Union
+from numpy.typing import ArrayLike, NDArray
+
+# Course Packages
+from DataManipulation import BBoxFormat
+
+# See https://docs.python.org/3/library/enum.html
+@unique
+class DiffMode(Enum):
+    # Type of data in the CSV
+    BACKWARD    = auto()
+    CENTRAL     = auto()
+    FORWARD     = auto()
+    COMPLEX     = auto()
+
+# Constants
+FIG_SIZE_DEF    = (8, 8)
+ELM_SIZE_DEF    = 50
+CLASS_COLOR     = ('b', 'r')
+EDGE_COLOR      = 'k'
+MARKER_SIZE_DEF = 10
+LINE_WIDTH_DEF  = 2
+
+def PlotBinaryClassData( mX: NDArray, vY: NDArray, /, *, hA: Optional[plt.Axes] = None, figSize: Tuple[int, int] = FIG_SIZE_DEF, 
+                        elmSize: int = ELM_SIZE_DEF, classColor: Tuple[str, str] = CLASS_COLOR, axisTitle: Optional[str] = None ) -> plt.Axes:
+    """
+    Plots binary 2D data as a scatter plot.
+    Input:
+        mX          - Matrix (numSamples, 2) of the data points.
+        vY          - Vector (numSamples) labels of the data (2 Distinct values only).
+    Output:
+        hA          - Axes handler the scatter was drawn on.
+    """
+
+    if hA is None:
+        hF, hA = plt.subplots(figsize = figSize)
+    else:
+        hF = hA.get_figure()
+    
+    vC, vN = np.unique(vY, return_counts = True)
+
+    numClass = len(vC)
+    if (len(vC) != 2):
+        raise ValueError(f'The input data is not binary, the number of classes is: {numClass}')
+
+    vIdx0 = vY == vC[0]
+    vIdx1 = vY == vC[1] #<! Basically ~vIdx0
+
+    hA.scatter(mX[vIdx0, 0], mX[vIdx0, 1], s = elmSize, color = classColor[0], edgecolor = 'k', label = f'$C_\u007b {vC[0]} \u007d$')
+    hA.scatter(mX[vIdx1, 0], mX[vIdx1, 1], s = elmSize, color = classColor[1], edgecolor = 'k', label = f'$C_\u007b {vC[1]} \u007d$')
+    hA.axvline(x = 0, color = 'k')
+    hA.axhline(y = 0, color = 'k')
+    hA.set_aspect('equal')
+    if axisTitle is not None:
+        hA.set_title(axisTitle)
+    hA.legend()
+    
+    return hA
+
+def Plot2DLinearClassifier( mX: NDArray, vY: NDArray, vW: NDArray, mX1: NDArray, mX2: NDArray, hA: plt.Axes ) -> None:
+    """
+    Plots a binary 2D classifier.
+    Input:
+        mX          - Matrix (3, numSamples) of the data points.
+        vY          - Vector (numSamples) labels of the data.
+        vW          - Vector (3) Parameters of the classifier.
+        mX1         - Matrix (numGridPtsX1, numGridPtsX2) of the grid points (1st variable).
+        mX2         - Matrix (numGridPtsX1, numGridPtsX2) of the grid points (2nd variable).
+        hA          - Axes handler the scatter was drawn X☻on.
+    Output:
+    Remarks:
+      - The model parameters `vW` match a matrix of the form: y_i = w_1 * x_1 + w_2 * x2 - w_0.
+      - The matrices `mX1` and `mX2` are the result of `np.meshgrid()` of 2D grid.
+    """
+    b = vW[0]
+    vW = vW[1:]
+    XX = np.column_stack([mX1.flatten(), mX2.flatten()])
+
+    vZ = (XX @ vW - b) > 0
+    ZZ = vZ.reshape(mX1.shape)
+    
+    vHatY    = np.sign(mX @ vW - b)
+    accuracy = np.mean(vY == vHatY)
+
+    axisTitle = r'$f_{{w},b} \left( {x} \right) = {sign} \left( {w}^{T} {x} - b \right)$' '\n' f'Accuracy = {accuracy:.2%}'
+
+    PlotBinaryClassData(mX, vY, hA = hA, axisTitle = axisTitle)
+    v = np.array([-2, 2])
+    hA.grid(True)
+    hA.plot(v, -(vW[0] / vW[1]) * v + (b / vW[1]), color = 'k', lw = 3)
+    hA.arrow(0, 0, vW[0], vW[1], color = 'orange', width = 0.05)
+    hA.axvline(x = 0, color = 'k', lw = 1)
+    hA.axhline(y = 0, color = 'k', lw = 1)
+    hA.contourf(mX1, mX2, ZZ, colors = CLASS_COLOR, alpha = 0.2, levels = [-0.5, 0.5, 1.5], zorder = 0)
+    
+    hA.set_xlim([-2, 2])
+    hA.set_ylim([-2, 2])
+    hA.set_xlabel('$x_1$')
+    hA.set_ylabel('$x_2$')
+
+    return
+
+def PlotMnistImages( mX: NDArray, vY: NDArray, numRows: int, numCols: Optional[int] = None, tuImgSize: Tuple = (28, 28), randomChoice: bool = True, lClasses: Optional[List] = None, hF: Optional[plt.Figure] = None ) -> plt.Figure:
+
+    numSamples  = mX.shape[0]
+    numPx       = mX.shape[1]
+
+    if numCols is None:
+        numCols = numRows
+
+    tFigSize = (numCols * 3, numRows * 3)
+
+    if hF is None:
+        hF, hA = plt.subplots(numRows, numCols, figsize = tFigSize)
+    else:
+        hA = hF.axes
+    
+    hA = np.atleast_1d(hA) #<! To support numImg = 1
+    hA = hA.flat
+    
+    for kk in range(numRows * numCols):
+        idx = np.random.choice(numSamples) if randomChoice else kk
+        mI  = np.reshape(mX[idx, :], tuImgSize)
+    
+        # hA[kk].imshow(mI.clip(0, 1), cmap = 'gray')
+        if len(tuImgSize) == 2:
+            hA[kk].imshow(mI, cmap = 'gray')
+        elif len(tuImgSize) == 3:
+            hA[kk].imshow(mI)
+        else:
+            raise ValueError(f'The length of the image size tuple is {len(tuImgSize)} which is not supported')
+        hA[kk].tick_params(axis = 'both', left = False, top = False, right = False, bottom = False, 
+                           labelleft = False, labeltop = False, labelright = False, labelbottom = False)
+        if lClasses is None:
+            hA[kk].set_title(f'Index = {idx}, Label = {vY[idx]}')
+        else:
+            hA[kk].set_title(f'Index = {idx}, Label = {lClasses[vY[idx]]}')
+    
+    return hF
+
+def PlotLabelsHistogram( vY: NDArray, hA: Optional[plt.Axes] = None, lClass: Optional[List] = None, xLabelRot: Optional[int] = None ) -> plt.Axes:
+
+    if hA is None:
+        hF, hA = plt.subplots(figsize = (8, 6))
+    
+    vLabels, vCounts = np.unique(vY, return_counts = True)
+
+    hA.bar(vLabels, vCounts, width = 0.9, align = 'center')
+    hA.set_title('Histogram of Classes / Labels')
+    hA.set_xlabel('Class')
+    hA.set_xticks(vLabels, [f'{labelVal}' for labelVal in vLabels])
+    hA.set_ylabel('Count')
+    if lClass is not None:
+        hA.set_xticklabels(lClass)
+    
+    if xLabelRot is not None:
+        for xLabel in hA.get_xticklabels():
+            xLabel.set_rotation(xLabelRot)
+
+    return hA
+
+def PlotConfusionMatrix(vY: NDArray, vYPred: NDArray, /, *, normMethod: str = None, hA: Optional[plt.Axes] = None, 
+                        lLabels: Optional[List] = None, dScore: Optional[Dict] = None, titleStr: str = 'Confusion Matrix', 
+                        xLabelRot: Optional[int] = None, valFormat: Optional[str] = None) -> Tuple[plt.Axes, NDArray]:
+
+    # Calculation of Confusion Matrix
+    mConfMat = confusion_matrix(vY, vYPred, normalize = normMethod)
+    oConfMat = ConfusionMatrixDisplay(mConfMat, display_labels = lLabels)
+    oConfMat = oConfMat.plot(ax = hA, values_format = valFormat)
+    hA = oConfMat.ax_
+    if dScore is not None:
+        titleStr += ':'
+        for scoreName, scoreVal in  dScore.items():
+            titleStr += f' {scoreName} = {scoreVal:0.2},'
+        titleStr = titleStr[:-1]
+    hA.set_title(titleStr)
+    hA.grid(False)
+    if xLabelRot is not None:
+        for xLabel in hA.get_xticklabels():
+            xLabel.set_rotation(xLabelRot)
+
+    return hA, mConfMat
+
+def PlotDecisionBoundaryClosure( numGridPts: int, gridXMin: float, gridXMax: float, gridYMin: float, gridYMax: float, /, *, clsColors: Tuple = CLASS_COLOR, numDigits: int = 1 ) -> Callable:
+
+    roundFctr = 10 ** numDigits
+    
+    # For equal axis
+    minVal = np.floor(roundFctr * min(gridXMin, gridYMin)) / roundFctr
+    maxVal = np.ceil(roundFctr * max(gridXMax, gridYMax)) / roundFctr
+    vX1    = np.linspace(minVal, maxVal, numGridPts)
+    vX2    = np.linspace(minVal, maxVal, numGridPts)
+    
+    mX1, mX2 = np.meshgrid(vX1, vX2)
+    mX       = np.c_[mX1.ravel(), mX2.ravel()] #<! Features (2D)
+
+    # A closure
+    def PlotDecisionBoundary(hDecFun: Callable, hA: plt.Axes = None) -> plt.Axes:
+        
+        if hA is None:
+            hF, hA = plt.subplots(figsize = (8, 6))
+
+        mZ = hDecFun(mX)
+        mZ = mZ.reshape(mX1.shape)
+
+        # Assumes values {0, 1}
+        hA.contourf(mX1, mX2, mZ, colors = clsColors, alpha = 0.3, levels = [-0.5, 0.5, 1.5])
+
+        return hA
+
+    return PlotDecisionBoundary
+
+def PlotRegressionData( mX: NDArray, vY: NDArray, /, *, hA: Optional[plt.Axes] = None, figSize: Tuple[int, int] = FIG_SIZE_DEF, elmSize: int = ELM_SIZE_DEF, classColor: Tuple[str, str] = CLASS_COLOR, axisTitle: Optional[str] = None ) -> plt.Axes:
+
+    if hA is None:
+        hF, hA = plt.subplots(figsize = figSize)
+    else:
+        hF = hA.get_figure()
+    
+    if np.ndim(mX) == 1:
+        mX = np.reshape(mX, (mX.size, 1))
+
+    numSamples = len(vY)
+    numDim     = mX.shape[1]
+    if (numDim > 2):
+        raise ValueError(f'The features data must have at most 2 dimensions')
+    
+    # Work on 1D, Add support for 2D when needed
+    # See https://matplotlib.org/stable/api/toolkits/mplot3d.html
+    hA.scatter(mX[:, 0], vY, s = elmSize, color = classColor[0], edgecolor = 'k', label = f'Samples')
+    hA.axvline(x = 0, color = 'k')
+    hA.axhline(y = 0, color = 'k')
+    hA.set_xlabel('${x}_{1}$')
+    # hA.axis('equal')
+    if axisTitle is not None:
+        hA.set_title(axisTitle)
+    hA.legend()
+    
+    return hA
+
+def PlotRegressionResults( vY, vYPred, /, *, hA: Optional[plt.Axes] = None, figSize: Tuple[int, int] = FIG_SIZE_DEF, lineWidth: int = LINE_WIDTH_DEF, elmSize: int = ELM_SIZE_DEF, classColor: Tuple[str, str] = CLASS_COLOR, axisTitle: Optional[str] = None ) -> plt.Axes:
+
+    if hA is None:
+        hF, hA = plt.subplots(figsize = figSize)
+    else:
+        hF = hA.get_figure()
+
+    numSamples = len(vY)
+    if (numSamples != len(vYPred)):
+        raise ValueError(f'The inputs `vY` and `vYPred` must have the same number of elements')
+    
+    hA.plot(vY, vY, color = 'r', lw = lineWidth, label = 'Ground Truth')
+    hA.scatter(vY, vYPred, s = elmSize, color = classColor[0], edgecolor = 'k', label = f'Estimation')
+    hA.set_xlabel('Label Value')
+    hA.set_ylabel('Prediction Value')
+    # hA.axis('equal')
+    if axisTitle is not None:
+        hA.set_title(axisTitle)
+    hA.legend()
+    
+    return hA
+
+def PlotScatterData( mX: NDArray, vL: Optional[NDArray] = None, hA: Optional[plt.Axes] = None, figSize: Tuple[int, int] = FIG_SIZE_DEF, markerSize: int = ELM_SIZE_DEF, edgeColor: int = EDGE_COLOR ) -> plt.Axes:
+
+    if hA is None:
+        hF, hA = plt.subplots(figsize = figSize)
+    else:
+        hF = hA.get_figure()
+    
+    numSamples = mX.shape[0]
+
+    if vL is None:
+        vL = np.zeros(numSamples)
+    
+    vU = np.unique(vL)
+    numClusters = len(vU)
+
+    for ii in range(numClusters):
+        vIdx = vL == vU[ii]
+        hA.scatter(mX[vIdx, 0], mX[vIdx, 1], s = markerSize, edgecolor = edgeColor, label = ii)
+    
+    hA.set_xlabel('${{x}}_{{1}}$')
+    hA.set_ylabel('${{x}}_{{2}}$')
+    hA.grid()
+    hA.legend()
+
+    return hA
+
+def PlotScatterData3D( mX: NDArray, vL: Optional[NDArray] = None, vC: Optional[NDArray] = None, axesProjection: Optional[str] = '3d', hA: Optional[plt.Axes] = None, figSize: Tuple[int, int] = FIG_SIZE_DEF ) -> plt.Axes:
+
+    if hA is None:
+        hF, hA = plt.subplots(figsize = figSize, subplot_kw = {'projection': axesProjection})
+    else:
+        hF = hA.get_figure()
+    
+    if vL is not None:
+        vU = np.unique(vL)
+        numClusters = len(vU)
+    else:
+        vL = np.zeros(mX.shape[0])
+        vU = np.zeros(1)
+        numClusters = 1
+
+    for ii in range(numClusters):
+        vIdx = vL == vU[ii]
+        if axesProjection == '3d':
+            hA.scatter(mX[vIdx, 0], mX[vIdx, 1], mX[vIdx, 2], s = ELM_SIZE_DEF, c = vC, alpha = 1, edgecolor = EDGE_COLOR, label = ii)
+        else:
+            hA.scatter(mX[vIdx, 0], mX[vIdx, 1], s = ELM_SIZE_DEF, c = vC, alpha = 1, edgecolor = EDGE_COLOR, label = ii)
+    
+    hA.set_xlabel('${{x}}_{{1}}$')
+    hA.set_ylabel('${{x}}_{{2}}$')
+    if axesProjection == '3d':
+        hA.set_zlabel('${{x}}_{{3}}$')
+    hA.grid()
+    hA.legend()
+
+    return hA
+
+def PlotDendrogram( dfX: Union[NDArray, pd.DataFrame], linkageMethod: str, valP: int, thrLvl: int, hA: Optional[plt.Axes] = None, figSize: Tuple[int, int] = FIG_SIZE_DEF ) -> plt.Axes:
+
+    if hA is None:
+        hF, hA = plt.subplots(1, 1, figsize = figSize)
+    else:
+        hF = hA.get_figure()
+
+    mLinkage = sp.cluster.hierarchy.linkage(dfX, method = linkageMethod)
+    sp.cluster.hierarchy.dendrogram(mLinkage, p = valP, truncate_mode = 'lastp', color_threshold = thrLvl, no_labels = True, ax = hA)
+    hA.axhline(y = thrLvl, c = 'k', lw = 2, linestyle = '--')
+
+    return hA
+
+def PlotBox( mI: NDArray, vLabel: Union[int, NDArray], mBox: NDArray, *, hA: Optional[plt.Axes] = None, lLabelText: Optional[List] = None ) -> plt.Axes:
+    # Assumes data in YOLO Format: [x, y, w, h] (Center, Height, Width)
+    
+    if hA is None:
+        dpi = 72
+        numRows, numCols = mI.shape[:2]
+        hF, hA = plt.subplots(figsize = (int(np.ceil(numCols / dpi) + 1), int(np.ceil(numRows / dpi) + 1)))
+    
+    hA.imshow(mI, extent = [0, 1, 1, 0]) #<! "Normalized Image"
+    hA.grid(False)
+
+    mBox = np.atleast_2d(mBox)
+    vLabel = np.atleast_1d(vLabel)
+    numObj = mBox.shape[0]
+    for ii in range(numObj):
+        if lLabelText is not None:
+            labelText = lLabelText[ii]
+        else:
+            labelText = '_'
+        PlotBBox(hA, vLabel[ii], mBox[ii], labelText)
+
+    return hA
+
+def PlotBBox( hA: plt.Axes, boxLabel: int, vBox: NDArray, labelText: str = '_' ) -> plt.Axes:
+    # Assumes data in YOLO Format
+    # Legend Text: https://stackoverflow.com/questions/24680981
+
+    edgeColor = hA._get_lines.get_next_color()
+
+    rectPatch = Rectangle((vBox[0] - (vBox[2] / 2), vBox[1] - (vBox[3] / 2)), vBox[2], vBox[3], linewidth = 2, edgecolor = edgeColor, facecolor = (0, 0, 0, 0), label = labelText) #<! Requires the alpha component in the face color
+    hA.add_patch(rectPatch)
+    hA.text(vBox[0] - (vBox[2] / 2), vBox[1] - (vBox[3] / 2), s = boxLabel, color = 'w', verticalalignment = 'bottom', bbox = {'color': edgeColor}, fontdict = {'size': 16})
+    hA.plot(vBox[0], vBox[1], 'x', mew = 5, ms = 10, color = edgeColor)
+
+    return hA
